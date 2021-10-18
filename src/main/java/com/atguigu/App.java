b0e28e228d7fc46939675c695cf6dbc4d8c4d520
Chen <Chen@qq.com>
2021-10-18 23:55:34 +0800

    second commit

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -7,5 +7,6 @@ package com.atguigu;
 public class App {
     public static void main( String[] args ) {
         System.out.println("aaaaaaaaaa");
+        System.out.println("bbbbbbbbbb");
     }
 }
